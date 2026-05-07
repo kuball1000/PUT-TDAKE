d020ff95a84d67b96ac9ff8dbc424f8a7bcf10be
kuball1000 <jk.buler@gmail.com>
2026-05-07 18:49:54 +0200

    Web Services (REST): ASP.NET Web API (08.05)

---
+++ CompanyApi/CompanyApi/Models/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyApi.Models;
+
+[Table("Employee")]
+public partial class Employee
+{
+    [Key]
+    public int EmployeeId { get; set; }
+
+    [StringLength(15)]
+    [Unicode(false)]
+    public string? FirstName { get; set; }
+
+    [StringLength(15)]
+    [Unicode(false)]
+    public string? LastName { get; set; }
+
+    public int? ManagerId { get; set; }
+
+    [Column(TypeName = "decimal(6, 2)")]
+    public decimal? Salary { get; set; }
+
+    [Column(TypeName = "decimal(6, 2)")]
+    public decimal? Bonus { get; set; }
+
+    public int? DepartmentId { get; set; }
+
+    [ForeignKey("DepartmentId")]
+    [InverseProperty("Employees")]
+    public virtual Department? Department { get; set; }
+
+    [ForeignKey("ManagerId")]
+    [InverseProperty(nameof(Employee.Subordinate))]
+    public virtual Employee? Manager { get; set; }
+    [InverseProperty(nameof(Employee.Manager))]
+    public virtual ICollection<Employee> Subordinate { get; set; }
+}
