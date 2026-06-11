dd41cac3ae78ca7502449449d14eb76ea3c05248
Jakub Buler <jk.buler@gmail.com>
2026-06-11 22:29:54 +0200

    dodanie zadania 12 (Angular + .NET Core Web API (12.06))

---
+++ StudentsApp/StudentsFront/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { AboutComponent } from './about/about.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+  { path: 'students', component: StudentsComponent },
+  { path: 'student-detail/:id', component: StudentDetailComponent },
+  { path: 'about', component: AboutComponent }
+];
